5abf7e6451b749de1e9b90194a2f0e7bfcd4e4e3
Mauro <mchiozzi95@gmail.com>
2018-02-23 21:40:00 -0300

    Added comments

---
+++ 7-segment-velocimeter/7-segment-velocimeter.ino
@@ -0,0 +1,119 @@
+/*
+	Code for seven segments velocimeter.
+	Feel free to use and edit.
+
+	Mauro Chiozzi 
+*/
+
+/* 
+	Seven segments (7s) display class. Could have used the already made lib,
+	but sometimes it's funnier DIY.
+*/
+class SevenSegmentsDisplay {	
+	// Array-sequence to turn up a digit
+	//							 	 A  B  C  D  E  F  G  H
+	const short numbers[10][8] =   {{1, 1, 1, 1, 1, 1, 0, 0}, 	// 0
+									{0, 1, 1, 0, 0, 0, 0, 0},	// 1
+									{1, 1, 0, 1, 1, 0, 1, 0},	// 2
+									{1, 1, 1, 1, 0, 0, 1, 0},	// 3
+									{0, 1, 1, 0, 0, 1, 1, 0},	// 4
+									{1, 0, 1, 1, 0, 1, 1, 0},	// 5
+									{1, 0, 1, 1, 1, 1, 1, 0},	// 6
+									{1, 1, 1, 0, 0, 0, 0, 0},	// 7
+									{1, 1, 1, 1, 1, 1, 1, 0},	// 8
+									{1, 1, 1, 0, 0, 1, 1, 0}};	// 9
+
+	// There are two kinds of 7s: the anode type and the cathode type
+	// Use 0 if your 7s use common anode or 0 if use common cathode.
+	boolean default_off_value;
+
+	/**
+		Port A 	=>	index 0;
+		Port B 	=>	index 1;
+		Port C 	=>	index 2;
+		Port D 	=>	index 3;
+		Port E 	=>	index 4;
+		Port F 	=>	index 5;
+		Port G 	=>	index 6;
+		Port DP =>	index 7; Can be omitted
+	*/
+	int *ports;
+	int ports_state[8];
+
+	public :
+
+		/**
+			Constructor. Must have the boolean value saying which type is the 7s
+		*/
+		SevenSegmentsDisplay(boolean _default_off_value){
+			default_off_value = _default_off_value;
+		}
+
+		/**
+			Set 7s ports according the relation shown above
+		*/
+		void setPorts(int *_ports){
+			ports = _ports;
+		}
+
+		/**
+			Aftes setting the ports, it's necessary initializate the ports as output
+		*/
+		void initDisplay(){
+			for(int i = 0; i < 7; i++){
+		 		pinMode(ports[i], OUTPUT);
+		 		digitalWrite(ports[i], default_off_value);
+			}
+		}
+
+		/**
+			Turn off all 7s leds
+		*/
+		void turnOffDisplay(){
+			for(int i = 0; i < 7; i++){
+		 		digitalWrite(ports[i], default_off_value);
+			}
+		}
+
+		/**
+			Turn on all 7s leds
+		*/
+		void turnOnDisplay(){
+			for(int i = 0; i < 7; i++){
+		 		digitalWrite(ports[i], !default_off_value);
+			}
+		}
+
+		/**
+			Write the number digit
+		*/
+		void writeDigit(int digit){
+			for(int i = 0; i < 7; i++){
+				digitalWrite(ports[i], numbers[digit][i] == !default_off_value);
+			}
+		}
+};
+
+SevenSegmentsDisplay displayA(1);
+SevenSegmentsDisplay displayB(1);
+
+void setup() {
+	int ports_displayA = new int[8]{35, 37, 27, 25, 23, 33, 31, 29};
+	int ports_displayB = new int[8]{34, 36, 26, 24, 22, 32, 30, 28};
+	
+	displayA.setPorts(ports_displayA);
+	displayB.setPorts(ports_displayB);
+
+	displayA.initDisplay();
+	displayB.initDisplay();
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+	for(int i = 0; i < 100 ; i++){
+ 		displayA.writeDigit(i / 10);
+ 		displayB.writeDigit(i % 10);
+ 		delay(400);
+	}
+}
+
